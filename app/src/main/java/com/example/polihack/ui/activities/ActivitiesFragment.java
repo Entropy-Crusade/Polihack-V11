e6435b6fa1baa9049255109ee6f60cd12f841934
Stefan Eduard <71097727+dotzzip@users.noreply.github.com>
2020-12-06 10:04:15 +0200

    gatat randomu

--- app/src/main/java/com/example/polihack/ui/activities/ActivitiesFragment.java
+++ app/src/main/java/com/example/polihack/ui/activities/ActivitiesFragment.java
@@ -14,22 +14,71 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.polihack.R;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ActivitiesFragment extends Fragment {
 
     private ActivitiesViewModel activitiesViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        activitiesViewModel =
-                new ViewModelProvider(this).get(ActivitiesViewModel.class);
+    public String hobby1() {
+
+        List<String> hobby1 = new ArrayList<>();
+
+        hobby1.add("H1_Activitate 1");
+        hobby1.add("H1_Activitate 2");
+        hobby1.add("H1_Activitate 3");
+        hobby1.add("H1_Activitate 4");
+        hobby1.add("H1_Activitate 5");
+        hobby1.add("H1_Activitate 6");
+        hobby1.add("H1_Activitate 7");
+        hobby1.add("H1_Activitate 8");
+        hobby1.add("H1_Activitate 9");
+        hobby1.add("H1_Activitate 10");
+
+
+        Collections.shuffle(hobby1);
+        return hobby1.get(0);
+    }
+
+    public String hobby2() {
+
+        List<String> hobby2 = new ArrayList<>();
+
+        hobby2.add("H2_Activitate 1");
+        hobby2.add("H2_Activitate 2");
+        hobby2.add("H2_Activitate 3");
+        hobby2.add("H2_Activitate 4");
+        hobby2.add("H2_Activitate 5");
+        hobby2.add("H2_Activitate 6");
+        hobby2.add("H2_Activitate 7");
+        hobby2.add("H2_Activitate 8");
+        hobby2.add("H2_Activitate 9");
+        hobby2.add("H2_Activitate 10");
+
+
+        Collections.shuffle(hobby2);
+        return hobby2.get(0);
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        activitiesViewModel = new ViewModelProvider(this).get(ActivitiesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_activities, container, false);
         final TextView textView = root.findViewById(R.id.text_activities);
+
+        TextView tv = root.findViewById(R.id.activ1);
+        tv.setText(String.valueOf(hobby1()));
+
+        TextView tv2 = root.findViewById(R.id.activ2);
+        tv2.setText(String.valueOf(hobby2()));
+
         activitiesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
+            @Override public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
         return root;
     }
+
 }
